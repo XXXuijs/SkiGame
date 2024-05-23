ac552b7c3706dcd75c625ae1a9a68f214a6a2cee
XXXuijs <sandisjaudzems@gmail.com>
2024-05-23 12:45:04 +0300

    cc

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Obstacle : MonoBehaviour
 {
+
+    private CinemachineImpulseSource impulseSource;
+
+    private void Start()
+    {
+        // Ensure the GameObject has a CinemachineImpulseSource component
+        impulseSource = GetComponent<CinemachineImpulseSource>();
+        if (impulseSource == null)
+        {
+            Debug.LogError("CinemachineImpulseSource component not found on this object.", gameObject);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision){
 
         if (collision.gameObject.tag == "Player"){
@@ -12,6 +26,12 @@ public class Obstacle : MonoBehaviour
     }
 
     public virtual void HitPlayer(GameObject player){
+
+        if (impulseSource != null)
+        {
+            impulseSource.GenerateImpulse();
+        }
+        
         Debug.Log("I hit the player");
         PlayerEvents.PlayerHit();
     }
